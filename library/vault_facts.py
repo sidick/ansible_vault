dc6ed3950761dcb924cf43251855ac636e01bec3
Simon Dick <simond@irrelevant.org>
2016-03-23 14:48:50 +0000

    Remove module parameter for make_vault_url as it's not used

--- library/vault_facts.py
+++ library/vault_facts.py
@@ -50,7 +50,7 @@ EXAMPLES = '''
 '''
 
 
-def make_vault_url(module, vault_server, vault_port, vault_tls):
+def make_vault_url(vault_server, vault_port, vault_tls):
     """ Create base Vault URL """
     vault_url = ''
     if vault_tls:
@@ -134,7 +134,7 @@ def main():
     vault_server = module.params['server']
     vault_tls = module.params['tls']
 
-    url = make_vault_url(module, vault_server, vault_port, vault_tls)
+    url = make_vault_url(vault_server, vault_port, vault_tls)
 
     vault_facts(module, url)
 
